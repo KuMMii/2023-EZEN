0d31ee7356c53421f229081623d4b6cd9492d619
KuMMii <4.prgmng@gmail.com>
2023-06-14 17:43:37 +0900

    sb

---
+++ SpringBoot_G11/src/main/java/com/ezen/g11/dao/IUserDao.java
@@ -0,0 +1,13 @@
+package com.ezen.g11.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.ezen.g11.dto.UserDto;
+
+@Mapper
+public interface IUserDao {
+
+	public List<UserDto> getList();
+}
---
+++ SpringBoot_G11/src/main/java/com/ezen/g11/dto/UserDto.java
@@ -0,0 +1,10 @@
+package com.ezen.g11.dto;
+
+import lombok.Data;
+
+@Data
+public class UserDto {
+
+	private String id;
+	private String name;
+}
